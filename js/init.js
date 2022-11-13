6a77b49602a306dc67b648555d7c9b681ec82732
PabloMartinPadilla <pablomartinpadilla@gmail.com>
2022-11-13 13:00:36 -0300

    E7

--- js/init.js
+++ js/init.js
@@ -1,49 +1,54 @@
 const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
-const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
+const PUBLISH_PRODUCT_URL =
+  "https://japceibal.github.io/emercado-api/sell/publish.json";
 const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
 const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
-const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
+const PRODUCT_INFO_COMMENTS_URL =
+  "https://japceibal.github.io/emercado-api/products_comments/";
 const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
-
-let getJSONData = function(url){
-    let result = {};
-    return fetch(url)
-    .then(response => {
+let getJSONData = function (url) {
+  let result = {};
+  return fetch(url)
+    .then((response) => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          return result;
+    .then(function (response) {
+      result.status = "ok";
+      result.data = response;
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = "error";
+      result.data = error;
+      hideSpinner();
+      return result;
     });
-}
+};
 
 /* Remplaza el nombre del usuario */
 
 document.addEventListener("DOMContentLoaded", function (e) {
   let eemail = localStorage.getItem("userEmail");
-
-  htmlContentToAppend = `<a>${eemail}</a>`
-
+  htmlContentToAppend = `<a>${eemail}</a>`;
   document.getElementById("showEmail").innerHTML = htmlContentToAppend;
-
 });
 
 /* funcion para borrar usuario */
 const logout = document.getElementById("cerrar-sesion");
-logout.addEventListener("click", function(){
-  localStorage.removeItem('userEmail');
-})
+logout.addEventListener("click", function () {
+  localStorage.removeItem("userEmail");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let email = localStorage.getItem("userEmail");
+  if (email == null) {
+    window.location = "index.html";
+  }
+});
--- js/my-profile.js
+++ js/my-profile.js
@@ -1,20 +1,56 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+let Nombre = document.getElementById("validationTooltip01");
+let Nombre2 = document.getElementById("validationTooltip02");
+let Apellido = document.getElementById("validationTooltip03");
+let Apellido2 = document.getElementById("validationTooltip04");
+let Telefono = document.getElementById("validationTooltip06");
+
+let submit = document.getElementById("submit");
+submit.addEventListener("click", () => {
+  let NombreV = Nombre;
+  let Nombre2V = Nombre2;
+  let ApellidoV = Apellido;
+  let Apellido2V = Apellido2;
+  let TelefonoV = Telefono;
+
+  localStorage.setItem("Nombre", NombreV.value);
+  localStorage.setItem("Nombre2", Nombre2V.value);
+  localStorage.setItem("Apellido", ApellidoV.value);
+  localStorage.setItem("Apellido2", Apellido2V.value);
+  localStorage.setItem("Telefono", TelefonoV.value);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let email = localStorage.getItem("userEmail");
+  if (email !== null) {
+    document.getElementById("validationTooltip05").value = email;
+    Nombre.value = localStorage.getItem("Nombre");
+    Nombre2.value = localStorage.getItem("Nombre2");
+    Apellido.value = localStorage.getItem("Apellido");
+    Apellido2.value = localStorage.getItem("Apellido2");
+    Telefono.value = localStorage.getItem("Telefono");
+  }
+});
