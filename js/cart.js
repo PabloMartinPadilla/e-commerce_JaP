2e0d31a9ac2941127df005c43fed33156e45c23a
PabloMartinPadilla <pablomartinpadilla@gmail.com>
2022-11-13 22:23:05 -0300

    Se hizo lo que se pudo

--- js/cart.js
+++ js/cart.js
@@ -1,33 +1,36 @@
 /*Boostrap*/
 (function () {
-      var forms = document.querySelectorAll('.needs-validation')
-      Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-    })()
-    /* Producto precargado*/
-const url = "https://japceibal.github.io/emercado-api/user_cart/" + 25801 + ".json";
+  var forms = document.querySelectorAll(".needs-validation");
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+/* Producto precargado*/
+const url =
+  "https://japceibal.github.io/emercado-api/user_cart/" + 25801 + ".json";
 
 const urlasync = async (url) => {
-    const fetchProm = await fetch(url);
-    if (fetchProm.ok) {
-      fetchResp = await fetchProm.json();
-      return fetchResp;
-    } 
+  const fetchProm = await fetch(url);
+  if (fetchProm.ok) {
+    fetchResp = await fetchProm.json();
+    return fetchResp;
+  }
 };
-
 
 document.addEventListener("DOMContentLoaded", async () => {
   const prod25801 = await urlasync(url);
-  let {name, currency, unitCost, count, image } = prod25801.articles[0];
+  let { name, currency, unitCost, count, image } = prod25801.articles[0];
   function getCartPreloadedProducts() {
     return `
     <div class="container tarjet rounded-3 p-4">
@@ -48,62 +51,69 @@ document.addEventListener("DOMContentLoaded", async () => {
           <td class="pt-5">${currency} ${unitCost}</td>
           <td class="pt-5">
           <input id="cantidad" class="form-control" type="number"  min="1" value="${count}" required></td>
-          <td class="pt-5" id="costoTotal"> ${currency} ${unitCost * count} </td>
+          <td class="pt-5" id="costoTotal"> ${currency} ${
+      unitCost * count
+    } </td>
         </tr>
       </tbody>
     </table>
   </div>
 `;
-}
-document.getElementById("escribir").innerHTML += getCartPreloadedProducts();
+  }
+  document.getElementById("escribir").innerHTML += getCartPreloadedProducts();
 
   /* Cuenta 
      Escribe SubTotal
   */
-let cantidad = document.getElementById("cantidad");
+  let cantidad = document.getElementById("cantidad");
   let Total = document.getElementById("costoTotal");
   document.getElementById("SubTotalCosto").innerHTML = Total.textContent;
+
   cantidad.addEventListener("change", () => {
     if (cantidad.value > 1) {
       let currentTotal = cantidad.value * prod25801.articles[0].unitCost;
-      return ((Total.innerHTML = currency + " " + currentTotal) (document.getElementById("SubTotalCosto").innerHTML = currency + " " + currentTotal));
-    } else{
-        cantidad.value = 1;
-        return ((Total.innerHTML = currency + " " + unitCost)(document.getElementById("SubTotalCosto").innerHTML = currency + " " + unitCost));
+      return (Total.innerHTML = currency + " " + currentTotal)(
+        (document.getElementById("SubTotalCosto").innerHTML =
+          currency + " " + currentTotal)
+      );
+    } else {
+      cantidad.value = 1;
+      return (Total.innerHTML = currency + " " + unitCost)(
+        (document.getElementById("SubTotalCosto").innerHTML =
+          currency + " " + unitCost)
+      );
     }
   });
-})
+});
 
 //modal
-nroTarjeta= document.getElementById('validationTooltip015');
-nroCuenta= document.getElementById('validationTooltip014');
-codSeguridad=document.getElementById('validationTooltip016'); 
-vencTarjeta=document.getElementById('validationTooltip017'); 
+nroTarjeta = document.getElementById("validationTooltip015");
+nroCuenta = document.getElementById("validationTooltip014");
+codSeguridad = document.getElementById("validationTooltip016");
+vencTarjeta = document.getElementById("validationTooltip017");
 
-document.getElementById('radioTarjeta').addEventListener('click', function (e) {
+document.getElementById("radioTarjeta").addEventListener("click", function (e) {
   nroTarjeta.disabled = false;
   codSeguridad.disabled = false;
-  vencTarjeta.disabled= false;
+  vencTarjeta.disabled = false;
   nroCuenta.disabled = true;
-
 });
-document.getElementById('radioBancaria').addEventListener('click', function (e) {
-  nroCuenta.disabled = false;
-  nroTarjeta.disabled = true;
-  codSeguridad.disabled = true;
-  vencTarjeta.disabled= true;
-});
-
+document.getElementById("radioBancaria").addEventListener("click", function (e) {
+    nroCuenta.disabled = false;
+    nroTarjeta.disabled = true;
+    codSeguridad.disabled = true;
+    vencTarjeta.disabled = true;
+  });
 
 const radioModal = document.querySelectorAll('input[name="radio-stacked2"]');
-const alertaPago= document.getElementById('alertaPago');
+const alertaPago = document.getElementById("alertaPago");
 
-document.getElementById('submit').addEventListener('click', function (e) {
-      if (radioModal.checked && (vencTarjeta.value == "") || (codSeguridad.value == "" && nroTarjeta.value == "" && nroCuenta.value == "")) {
-         alertaPago.innerHTML= ` <p class="text-danger">Debe ingresar un método de pago </p>`
-      }
-     else{
-      alertaPago.innerHTML= ``
-     }
-})
-
+document.getElementById("submit").addEventListener("click", function (e) {
+  if (
+    (radioModal.checked && vencTarjeta.value == "") ||(codSeguridad.value == "" && nroTarjeta.value == "" && nroCuenta.value == "")
+  ) {
+    alertaPago.innerHTML = ` <p class="text-danger">Debe ingresar un método de pago </p>`;
+  } else {
+    alertaPago.innerHTML = ``;
+  }
+});
